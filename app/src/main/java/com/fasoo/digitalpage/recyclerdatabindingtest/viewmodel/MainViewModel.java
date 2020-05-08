2560c112c8ddae4ed154e5a2f430eddcea9e68e6
jun-pil park <iamfeel2@gmail.com>
2020-05-08 17:59:57 +0900

    feat (MainViewModel) : Repository 로 Data 가지고 오도록 변경

--- app/src/main/java/com/fasoo/digitalpage/recyclerdatabindingtest/viewmodel/MainViewModel.java
+++ app/src/main/java/com/fasoo/digitalpage/recyclerdatabindingtest/viewmodel/MainViewModel.java
@@ -1,28 +1,42 @@
 package com.fasoo.digitalpage.recyclerdatabindingtest.viewmodel;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.fasoo.digitalpage.recyclerdatabindingtest.repository.MovieDataSource;
+import com.fasoo.digitalpage.recyclerdatabindingtest.repository.MovieRepository;
 import com.fasoo.digitalpage.recyclerdatabindingtest.repository.data.Movie;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainViewModel extends ViewModel {
     private MutableLiveData<List<Movie>> _movies = new MutableLiveData<>();
-    private List<Movie> movies = new ArrayList<>();
+    private MovieRepository movieRepository;
+
+    public MainViewModel(MovieRepository repository) {
+        movieRepository = repository;
+        loadData();
+    }
+
+    private void loadData() {
+        movieRepository.getMovies(new MovieDataSource.LoadMoviesCallback() {
+            @Override
+            public void onMovieLoaded(List<Movie> movie) {
+                _movies.postValue(movie);
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+                Log.e("jppark", "[MainViewModel - onDataNotAvailable]: Movie Data load Error in movie Repository. ");
+            }
+        });
+    }
 
     public LiveData<List<Movie>> getMovies() {
         return _movies;
     }
 
-    public void addMovie() {
-        movies.add(new Movie("test" + getRandom(), "test1, test2", "test000"));
-        _movies.setValue(movies);
-    }
-
-    private long getRandom() {
-        return Math.round((Math.random() * 1000) / 10);
-    }
 }
